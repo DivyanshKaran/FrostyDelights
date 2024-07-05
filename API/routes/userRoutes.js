91c61968a7b59f28e8555de6e89c9673d9a874c3
DivyanshKaran <karandivyansh01@gmail.com>
2024-07-06 00:59:52 +0530

    Initialized a new project

--- API/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authenticationController");
-
-router.post("/signup", authController.signUp);
-router.post("/login", authController.login);
-
-module.exports = router;
